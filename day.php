27d422d71a49fb64922952271e80e179bd74acf3
Daniel Write <daniel@writefamily.com>
2022-09-27 14:08:40 -0400

    ADD: base api functions

---
+++ day.php
@@ -0,0 +1,4 @@
+<?php
+# accept paramter of date in form (mm-dd-yyyy)
+# return JSON with filtered liturgical options for that day
+?>
---
+++ year.php
@@ -0,0 +1,6 @@
+<?php
+# accept paramter year=(yyyy)
+# return all liturgical options for every day of that year
+# include temporal and sanctoral
+header('Content-type: text/json',true);
+?>
